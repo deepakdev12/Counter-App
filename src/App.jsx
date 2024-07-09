3a6fce2715b9e38e25085cb2c92ece3c87ba8e0f
deepakdev12 <159275278+deepakdev12@users.noreply.github.com>
2024-07-09 15:25:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Counter from "./local.jsx";  // Make sure the path is correct based on your project structure
+
+function App() {
+    return (
+        <div className="counter-container">
+            <h1>Simple Counter App</h1>
+            <Counter />
+        </div>
+    );
+}
+
+export default App;
